4e3a5ea55ef8cae90d26fe6ea7d89bcabd8c374c
Abhishek Gangwar <me.abhishekgangwar@gmail.com>
2021-03-16 00:42:45 +0530

    feat: added intersection observer changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [element, setElement] = React.useState(null);
+
+  const handleInterSectionCallback = (entries, observer) =>{
+    entries.forEach(element => {
+      if(element.isIntersecting){
+        setIsLoading(true)
+      } else{
+        setIsLoading(false)
+      }
+    });
+  }
+
+  const observer = new IntersectionObserver(handleInterSectionCallback,{
+    threshold: 1
+  })
+
+
+  React.useEffect(() =>{
+    let elm = element
+    if(elm){
+      observer.observe(elm);
+    }
+
+    return () =>{
+      if(elm){
+        observer.unobserve(elm)
+      }
+    }
+  },[element])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Project Infinite Scroll
       </header>
+      <main className="App-main">
+        <section className="infinite-target">
+          This is the building block and main target element.
+        </section>
+        <div ref={setElement} id="target">
+         second block
+        </div>
+        {isLoading && <div className="loading">'Loading .......'</div>}
+      </main>
     </div>
   );
 }
